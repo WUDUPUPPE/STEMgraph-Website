7279561a02bc9a4acb3c13f07348a7a9d28e4e22
WUDUPUPPE <swizz020@icloud.com>
2026-08-27 23:19:54 +0200

    add home, keywords route path and generate keywords page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,21 @@
 import { Routes } from '@angular/router';
+import { MainLayout } from './layouts/main-layout/main-layout';
 
 export const routes: Routes = [
   {
-    path: '', loadComponent: () =>
-        import('./pages/home/home').then(m => m.Home)
+    path: '',
+    component: MainLayout,
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('./pages/home/home').then(m => m.Home)
+      },
+      {
+        path: 'keywords',
+        loadComponent: () =>
+          import('./pages/keywords/keywords').then(m => m.Keywords)
+      }
+    ]
   }
 ];
---
+++ src/app/pages/keywords/keywords.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-keywords',
+  styleUrl: './keywords.css',
+  templateUrl: './keywords.html',
+})
+export class Keywords {}
