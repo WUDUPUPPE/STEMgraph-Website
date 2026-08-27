7e4ec5f895182f4bcfba02efec83470a6407ac3d
WUDUPUPPE <swizz020@icloud.com>
2026-08-27 20:31:44 +0200

    add navigation and router outlet

--- src/app/layouts/main-layout/main-layout.ts
+++ src/app/layouts/main-layout/main-layout.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from "../../../../node_modules/@angular/router/types/_router_module-chunk";
 
 @Component({
-  imports: [],
+  imports: [RouterOutlet],
   selector: 'app-main-layout',
   styleUrl: './main-layout.css',
   templateUrl: './main-layout.html',
