98b9059041e58ef044f7835d98a10cd290ae32ef
WUDUPUPPE <swizz020@icloud.com>
2026-09-02 16:31:27 +0200

    add serach and filter options

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { Router, RouterLink } from "@angular/router";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {}
+export class Navbar {
+  searchTerm = '';
+  searchType = 'all';
+
+  constructor(private readonly router: Router) {}
+
+  submitSearch(): void {
+    const query = this.searchTerm.trim();
+
+    if (!query) {
+      return;
+    }
+
+    this.router.navigate(['/search'], {
+      queryParams: {
+        q: query,
+        type: this.searchType
+      }
+    });
+  }
+}
