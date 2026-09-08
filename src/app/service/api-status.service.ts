3dca9cc66318f54991865c582691bd4a1a96d41e
WUDUPUPPE <swizz020@icloud.com>
2026-09-08 20:19:43 +0200

    add status info

---
+++ src/app/service/api-status.service.ts
@@ -0,0 +1,23 @@
+import { Injectable, inject, signal } from "@angular/core";
+import { STEMgraphApiService } from "./stemgraph-api.service";
+
+export type ApiStatus = 'checking' | 'online' | 'offline';
+
+@Injectable({
+  providedIn: 'root',
+})
+
+export class ApiStatusService {
+  private readonly stemgraphApi = inject(STEMgraphApiService);
+
+  readonly status = signal<ApiStatus>('checking');
+
+  check(): void {
+    this.status.set('checking');
+
+    this.stemgraphApi.healthcheck().subscribe({
+      next: () => this.status.set('online'),
+      error: () => this.status.set('offline'),
+    });
+  }
+}
