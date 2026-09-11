b37d4fcef65d5bd296ec1ffa4818f4715100d643
WUDUPUPPE <swizz020@icloud.com>
2026-09-11 08:52:01 +0200

    add root URL

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,7 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
+import { provideApiConfiguration } from './api/api-configuration';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -10,5 +11,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideClientHydration(),
     provideHttpClient(),
+    provideApiConfiguration('http://localhost:8000')
   ],
 };
