94750f3ed3404154447b5d7171a6eac9b0579a69
WUDUPUPPE <swizz020@icloud.com>
2026-09-11 08:58:50 +0200

    fix to use generated api code

--- src/app/service/stemgraph-api.service.ts
+++ src/app/service/stemgraph-api.service.ts
@@ -1,21 +1,26 @@
 import { Injectable, inject } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { from } from 'rxjs';
+import { Api } from '../api/api';
 import { healthcheckHealthcheckGet } from "../api/functions";
 import { getGraphGraphGet } from "../api/functions";
+import { getListListGet } from "../api/functions";
 
 @Injectable({
   providedIn: 'root',
 })
 
 export class STEMgraphApiService {
-  private readonly http = inject(HttpClient);
-  private readonly rootUrl = 'http://localhost:8000';
+  private readonly api = inject(Api);
 
   Healthcheck() {
-    return healthcheckHealthcheckGet(this.http, this.rootUrl);
+    return from(this.api.invoke(healthcheckHealthcheckGet));
   }
 
   MainGraph() {
-    return getGraphGraphGet(this.http, this.rootUrl);
+    return from(this.api.invoke(getGraphGraphGet));
+  }
+
+  MainLists() {
+    return from(this.api.invoke(getListListGet));
   }
 }
