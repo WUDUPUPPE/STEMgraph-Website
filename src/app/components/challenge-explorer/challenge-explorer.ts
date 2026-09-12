72970b084fadf281dc91918a3b313f7231cb6835
WUDUPUPPE <swizz020@icloud.com>
2026-09-12 15:08:36 +0200

    add graph info view

--- src/app/components/challenge-explorer/challenge-explorer.ts
+++ src/app/components/challenge-explorer/challenge-explorer.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, inject, input, signal } from '@angular/core';
 import { STEMgraphApiService } from '../../service/stemgraph-api.service';
-import { ChallengeListResponse } from '../../api/models';
+import { ChallengeListResponse, GraphResponse } from '../../api/models';
 
 type ExplorerView = 'graph' | 'list';
 
@@ -16,13 +16,16 @@ export class ChallengeExplorer implements OnInit {
   readonly isAdmin = input(false);
 
   protected readonly viewMode = signal<ExplorerView>('graph');
-  protected readonly challenges = signal<ChallengeListResponse[]>([]);
   protected readonly isLoading = signal(false);
   protected readonly errorMessage = signal<string | null>(null);
   protected readonly openedChallengeId = signal<string | null>(null);
 
+  protected readonly list = signal<ChallengeListResponse[]>([]);
+  protected readonly graph = signal<GraphResponse | null>(null);
+
+
   ngOnInit(): void {
-    this.loadList();
+    this.loadGraph();
   }
 
   protected showList(): void {
@@ -32,6 +35,10 @@ export class ChallengeExplorer implements OnInit {
 
   protected showGraph(): void {
     this.viewMode.set('graph');
+
+    if (this.graph() === null) {
+    this.loadGraph();
+    }
   }
 
   protected toggleChallenge(challengeId: string): void {
@@ -39,19 +46,37 @@ export class ChallengeExplorer implements OnInit {
     currentId === challengeId ? null : challengeId);
   }
 
-  private loadList(): void{
+  private loadList(): void {
     this.isLoading.set(true);
     this.errorMessage.set(null);
 
     this.stemgraphApi.MainList().subscribe({
       next: (response) => {
-        this.challenges.set(response);
+        this.list.set(response);
         console.log('Main list:', response);
         this.isLoading.set(false);
       },
       error: (error) => {
         console.error('Challenge list could not be loaded:', error);
-        this.errorMessage.set('Challenge list could not be loaded');
+        this.errorMessage.set('List could not be loaded');
+        this.isLoading.set(false);
+      },
+    });
+  }
+
+  private loadGraph(): void {
+    this.isLoading.set(true);
+    this.errorMessage.set(null);
+
+    this.stemgraphApi.MainGraph().subscribe({
+      next: (response) => {
+        this.graph.set(response);
+        console.log('Maingraph:', response);
+        this.isLoading.set(false);
+      },
+      error: (error) => {
+        console.error('Main graph could not be loaded:', error);
+        this.errorMessage.set('Graph could not be loaded');
         this.isLoading.set(false);
       },
     });
